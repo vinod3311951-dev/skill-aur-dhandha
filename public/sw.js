0607f410b947d92ccbf69b895dccdb2a7ee021f8
vinod3311951-dev <vinod3311951@gmail.com>
2026-09-18 15:50:13 +0530

    pwa: precache install icons with shell

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE='skill-aur-dhandha-v4';
-const SHELL=['/','/index.html','/manifest.webmanifest','/icon.svg'];
+const CACHE='skill-aur-dhandha-v5';
+const SHELL=['/','/index.html','/manifest.webmanifest','/icon.svg','/icon-192.png','/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{
